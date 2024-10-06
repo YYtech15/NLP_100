edae2ee4b839309a5542430b6b57f7106eed628b
YYtech15 <midori1140flower@gmail.com>
2024-10-06 15:03:12 +0900

    答え方の修正

--- 01_準備運動/1.py
+++ 01_準備運動/1.py
@@ -2,6 +2,11 @@
 # 00. 文字列の逆順
 # 文字列”stressed”の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
 
-word = "stressed"
+def reverse_string(word):
+    return word[::-1]
 
-print("ans:", word[::-1])
+if __name__ == "__main__":
+    word = "stressed"
+    
+    ans = reverse_string(word)
+    print("ans:", ans)
--- 01_準備運動/2.py
+++ 01_準備運動/2.py
@@ -2,6 +2,11 @@
 # 01. 「パタトクカシーー」
 # 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
 
-word = "パタトクカシーー"
+def extract_odd_index_chars(word):
+    return word[0::2]
 
-print("ans:", word[0::2])
+if __name__ == "__main__":
+    word = "パタトクカシーー"
+    
+    ans = extract_odd_index_chars(word)
+    print("ans:", ans)
