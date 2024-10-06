858e20d8d8aae10672063065c06fd6bc6fdac2d9
YYtech15 <midori1140flower@gmail.com>
2024-10-06 15:30:48 +0900

    03問題に回答

--- 01_準備運動/4.py
+++ 01_準備運動/4.py
@@ -1,1 +1,17 @@
 # 4.py 
+# 03. 円周率
+# “Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.”
+# という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+import re
+
+def word_lengths(sentence):
+    # words = sentence.replace(",","").replace(".","").split(" ")
+    # 正規表現で句読点をすべて削除
+    words = re.sub(r'[^\w\s]', '', sentence).split(" ")
+    return [len(word) for word in words]
+
+if __name__ == "__main__":
+    sentence = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+    
+    ans = word_lengths(sentence)
+    print("ans:", ans)
