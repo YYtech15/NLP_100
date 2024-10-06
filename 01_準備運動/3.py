052b7f92741a2f04d75ffb443c8f1f01c8f90602
YYtech15 <midori1140flower@gmail.com>
2024-10-06 15:12:08 +0900

    02問題に回答

--- 01_準備運動/3.py
+++ 01_準備運動/3.py
@@ -1,1 +1,14 @@
 # 3.py 
+# 02. 「パトカー」＋「タクシー」＝「パタトクカシーー」
+# 「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
+
+def alternate_merge_strings(word_1, word_2):
+    merge_words = ''.join([char_1 + char_2 for char_1, char_2 in zip(word_1, word_2)])
+    return merge_words
+
+if __name__ == "__main__":
+    word_1 = "パトカー"
+    word_2 = "タクシー"
+    
+    ans = alternate_merge_strings(word_1, word_2)
+    print("ans:", ans)
